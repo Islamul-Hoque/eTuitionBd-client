26082abd2cacce184aa74e2c513065accf3be920
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-12-12 01:55:22 +0600

    feat(TuitionManagement-ui): support tuition rejection flow with SweetAlert2 confirmation and status update

--- src/pages/Dashboard/TuitionManagement/TuitionManagement.jsx
+++ src/pages/Dashboard/TuitionManagement/TuitionManagement.jsx
@@ -67,7 +67,7 @@ const TuitionManagement = () => {
                 <td>{tuition.status}</td>
                 <td className="flex gap-2">
                     <button onClick={() => handleStatusChange(tuition._id, "Approved")} className="btn btn-sm bg-indigo-600 text-white hover:bg-indigo-700 flex items-center gap-2 shadow-md"> <HiCheckCircle className="text-lg" /> Approve</button>
-                    {/* <button onClick={() => handleStatusChange(tuition._id, "Rejected")} className="btn btn-sm bg-indigo-100 text-gray-800 hover:bg-indigo-200 flex items-center gap-2 shadow-md"><HiXCircle className="text-lg" /> Reject</button> */}
+                    <button onClick={() => handleStatusChange(tuition._id, "Rejected")} className="btn btn-sm bg-indigo-100 text-gray-800 hover:bg-indigo-200 flex items-center gap-2 shadow-md"><HiXCircle className="text-lg" /> Reject</button>
                 </td>
               </tr>
             ))}
